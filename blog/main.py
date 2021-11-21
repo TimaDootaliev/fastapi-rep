71937f41e59a27f1449ea318a25ca723dc277db5
timur <timur.dootaliev.td@gmail.com>
2021-11-21 22:23:36 +0600

    post CRUD

--- blog/main.py
+++ blog/main.py
@@ -1,3 +1,4 @@
+from typing import List
 from fastapi import (FastAPI, Depends, status, Response, HTTPException)
 from . import schemas, models
 from .database import Base, engine, SessionLocal
@@ -18,7 +19,8 @@ def get_db():
 
 
 @app.post('/blog', status_code=status.HTTP_201_CREATED)
-async def create_post(request: schemas.Blog, db: Session = Depends(get_db)):
+def create_post(request: schemas.Blog, db: Session = Depends(get_db)):
+    """ Создание поста """
     new_blog = models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
     db.commit()
@@ -26,19 +28,42 @@ async def create_post(request: schemas.Blog, db: Session = Depends(get_db)):
     return new_blog
 
 
-@app.get('/blog')
+@app.get('/blog', response_model=List[schemas.ShowBlog])
 def post_list(db: Session = Depends(get_db)):
-    """ Функция для просмотра всех записей """
+    """ Просмотр всех записей """
     blogs = db.query(models.Blog).all()
     return blogs
 
 
-@app.get('/blog/{id}', status_code=200)
-def post_detail(
-                id, 
-                response: Response, 
-                db: Session = Depends(get_db)
-                ):
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def destroy_post(id, db: Session = Depends(get_db)):
+    """ Удаление записей """
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f'Post with the id {id} doesn\'t exist'
+        )
+    blog.delete(synchronize_session=False)
+    db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update_post(id, request: schemas.Blog, db: Session = Depends(get_db)):
+    """ Изменение поста """
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Post with id {id} not found")
+
+    blog.update(request.dict())
+    db.commit()
+    return f'Post with id {id} updated'
+
+
+@app.get('/blog/{id}', status_code=200, response_model=schemas.ShowBlog)
+def post_detail(id, response: Response, db: Session = Depends(get_db)):
     """ Функция для просмотра деталей поста """
     blog = db.query(models.Blog).filter(models.Blog.id==id).first()
     if not blog:
@@ -49,3 +74,16 @@ def post_detail(
         # response.status_code = status.HTTP_404_NOT_FOUND
         # return {'detail': f'Post with the id {id} doesn\'t exist'}
     return blog
+
+
+
+@app.post('/user')
+def create_user(request: schemas.User, 
+                db: Session = Depends(get_db)):
+    new_user = models.User(name=request.name, email=request.email, password=request.password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+# TODO: 2:23:40 password hashing
